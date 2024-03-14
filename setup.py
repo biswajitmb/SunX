981884d1084dcb28a39d57a551482f46d3c79d1a
biswajitmb <65720146+biswajitmb@users.noreply.github.com>
2024-03-14 15:19:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='arsim',
       version='0.1',
-      description='arsim: A Python Package for Modeling coronal loops',
+      description='SunX: A Python Package for Modeling coronal loops',
       author='Biswajit Mondal',
       author_email='biswajit.mondal@nasa.gov',
       #url='https://github.com/biswajitmb/DarpanX/tree/objOriented/objectOriented/python',
